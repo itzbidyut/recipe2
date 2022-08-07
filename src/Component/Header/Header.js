702ee50db9163ed98bfef22e8f7edcd200ed311d
itzbidyut <bbboruah1994@gmail.com>
2022-08-07 11:24:25 +0530

    first commit

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export default function Header() {
+  return <Conatiner>Header</Conatiner>;
+}
+
+const Conatiner = styled.div`
+  padding: 0% 20%;
+  background: red;
+`;
